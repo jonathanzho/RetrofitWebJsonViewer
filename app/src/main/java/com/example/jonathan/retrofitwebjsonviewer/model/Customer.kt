683ff09277b471c035135bd991e75f964e619595
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-10-15 00:14:56 -0700

    from Customer.java to Customer.kt; from Friend.java to Frient.kt

---
+++ app/src/main/java/com/example/jonathan/retrofitwebjsonviewer/model/Customer.kt
@@ -0,0 +1,29 @@
+package com.example.jonathan.retrofitwebjsonviewer.model
+
+import com.example.jonathan.retrofitwebjsonviewer.model.Friend
+import java.util.ArrayList
+
+class Customer(
+    var name: String,
+    var email: String,
+    var balance: String,
+    var friends: ArrayList<Friend>
+) {
+    private val _id: String? = null
+    private val index = 0
+    private val guid: String? = null
+    private val isActive = false
+    private val picture: String? = null
+    private val age = 0
+    private val eyeColor: String? = null
+    private val gender: String? = null
+    private val company: String? = null
+    private val phone: String? = null
+    private val address: String? = null
+    private val about: String? = null
+    private val registered: String? = null
+    private val latitude = 0f
+    private val longitude = 0f
+    private val tags: ArrayList<String>? = null
+
+}
---
+++ app/src/main/java/com/example/jonathan/retrofitwebjsonviewer/model/Friend.kt
@@ -0,0 +1,6 @@
+package com.example.jonathan.retrofitwebjsonviewer.model
+
+class Friend {
+    var id = 0
+    var name: String? = null
+}
